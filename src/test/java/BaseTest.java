ea7a9da50383c32f8ba18a2b6978613ad7d3236c
jhonatansilvadesousa <jhonatan2760@gmail.com>
2019-11-15 17:35:34 -0300

    Addng test example

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,9 @@
+
+/*
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWireMock(port = 9195)
+@ActiveProfiles("test")*/
+public abstract class BaseTest {
+
+}
